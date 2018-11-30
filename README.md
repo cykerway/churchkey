fdbe0ff04937ca26389445fd6fb19840d04db25f
Cyker Way <cykerway@gmail.com>
2018-11-30 04:03:29 -0500

    -

---
+++ README.md
@@ -0,0 +1,61 @@
+# churchkey
+
+a tool tunneling ssh over http proxy;
+
+churchkey tunnels ssh sessions through http proxy, so that users can log into
+remote server via http proxy if direct access is not working; churchkey uses
+http *connect* method and only works with http proxies supporting this method;
+
+`churchkey` supports two http authentication methods: [basic] and [digest];
+
+technical details are described in [rfc 2817];
+
+alternative projects include [corkscrew] and [proxytunnel];
+
+## install
+
+install via pip:
+
+    pip install churchkey
+
+## usage
+
+1.  create a file `~/.ssh/proxyauth` with proxy username and password:
+
+        <username>:<password>
+
+2.  edit `~/.ssh/config` as follows:
+
+        Host {host}
+            HostName {hostname}
+            Port {port}
+            User {user}
+            ProxyCommand churchkey {proxy_host} {proxy_port} %h %p ~/.ssh/proxyauth
+
+3.  log into remote server as usual:
+
+        ssh {host}
+
+## license
+
+Copyright (C) 2016-2018 Cyker Way
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+[rfc 2817]: https://tools.ietf.org/html/rfc2817
+[basic]: https://en.wikipedia.org/wiki/Basic_access_authentication
+[digest]: https://en.wikipedia.org/wiki/Digest_access_authentication
+[corkscrew]: http://agroman.net/corkscrew/
+[proxytunnel]: http://proxytunnel.sourceforge.net/
+
--- churchkey/__main__.py
+++ churchkey/__main__.py
@@ -1,28 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 '''
-churchkey: A program tunneling SSH through HTTP proxies with authentication.
-
-Copyright (C) 2016 Cyker Way
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+a tool tunneling ssh over http proxy;
 '''
 
+from os.path import expanduser
+from os.path import isfile
 import argparse
 import base64
 import hashlib
 import http.client
+import logging
 import os
 import random
 import select
@@ -30,268 +18,306 @@ import socket
 import sys
 import traceback
 
-# Program version.
-VERSION = '1.0.0'
+##  global config;
+class conf: pass
 
-# Buffer size for read and write.
-BUFSIZE = 4096
+def md5(s):
 
-# Global config.
-config = {}
-
-def md5(str_):
     '''
-    Return MD5 checksum of the given string.
+    return md5 checksum of given string;
 
-    Parameters
-    ----------
-    plaintext
-        Input string.
+    ## params
 
-    Returns
-    -------
-    str_ : str
-        MD5 checksum in hex format.
+    s:str
+    :   input string;
+
+    ## return
+
+    :str
+    :   md5 checksum in hex format;
     '''
-    return hashlib.md5(str_.encode()).hexdigest()
+
+    return hashlib.md5(s.encode()).hexdigest()
 
 def parse_args():
+
     '''
-    Parse command-line arguments.
+    parse command line arguments;
     '''
+
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
-        'proxy_host', type=str, default='127.0.0,1', help='proxy host')
+        'proxy_host',
+        default='127.0.0,1',
+        help='proxy host;',
+    )
+
     parser.add_argument(
-        'proxy_port', type=int, default=3128, help='proxy port')
+        'proxy_port',
+        type=int,
+        default=3128,
+        help='proxy port;',
+    )
+
     parser.add_argument(
-        'dest_host', type=str, default='127.0.0.1', help='dest host')
+        'dest_host',
+        default='127.0.0.1',
+        help='dest host;',
+    )
+
     parser.add_argument(
-        'dest_port', type=int, default=22, help='dest port')
+        'dest_port',
+        type=int,
+        default=22,
+        help='dest port;',
+    )
+
     parser.add_argument(
-        'auth_file', type=str, default=os.path.expanduser('~/.ssh/proxyauth'),
-        help='auth file')
+        'auth_file',
+        default=expanduser('~/.ssh/proxyauth'),
+        help='auth file;',
+    )
+
     parser.add_argument(
-        '--verbose', action='store_true', help='display verbose output')
+        '--buf-size',
+        type=int,
+        default=4096,
+        help='data buffer size;',
+    )
+
     parser.add_argument(
-        '--version', action='version', version='%(prog)s {}'.format(VERSION),
-        help='show version')
+        '-d', '--debug',
+        action='store_true',
+        help='enable debug mode;',
+    )
 
     args = parser.parse_args()
 
-    config['parser'] = parser
-    config['proxy_host'] = args.proxy_host
-    config['proxy_port'] = args.proxy_port
-    config['dest_host'] = args.dest_host
-    config['dest_port'] = args.dest_port
-    if os.path.isfile(args.auth_file):
-        with open(args.auth_file, 'rt') as f:
-            username, password = f.readline().strip().split(':')
-        config['username'] = username
-        config['password'] = password
-    elif os.path.exists(args.auth_file):
-        raise Exception('{} is not a regular file'.format(args.auth_file))
-    config['verbose'] = args.verbose
+    return args
 
-def probe():
-    '''
-    Probe the proxy to see whether authentication is needed.
+def probe_proxy():
 
-    Returns
-    -------
-    HTTPResponse
-        An HTTP response object.
     '''
-    conn = http.client.HTTPConnection(config['proxy_host'],
-                                      config['proxy_port'])
-    conn.request('CONNECT',
-                 config['dest_host'] + ':' + str(config['dest_port']))
+    probe the proxy to check if authentication is needed;
+
+    ## return
+
+    :HTTPResponse
+    :   http response object;
+    '''
+
+    conn = http.client.HTTPConnection(conf.proxy_host, conf.proxy_port)
+    conn.request('CONNECT', conf.dest_host + ':' + str(conf.dest_port))
     resp = conn.getresponse()
     conn.close()
     return resp
 
-def parse_auth_header(auth_header):
-    '''
-    Parse authentication header in proxy response.
+def parse_auth_header(auth):
 
-    Parameters
-    ----------
-    auth_header
-        The header to parse.
-
-    Returns
-    -------
-    scheme : str
-        The auth scheme (basic or digest).
-    fields : dict
-        A dict of header fields.
     '''
-    scheme, fields_str = auth_header.split(' ', 1)
+    parse authentication header in proxy response;
+
+    ## params
+
+    auth:str
+    :   authentication header;
+
+    ## return
+
+    scheme:str
+    :   authentication scheme (basic or digest);
+
+    fields:dict
+    :   a dict of header fields;
+    '''
+
+    scheme, fields_str = auth.split(' ', 1)
     scheme = scheme.lower()
     fields = {}
     for item in fields_str.split(', '):
         key, value = item.split('=')
         key = key.lower()
         if key in ['realm', 'nonce', 'qop']:
-            value = value[1:-1].replace('\"', '"')  # unquote
+            value = value[1:-1].replace('\"', '"')
         elif key in ['stale']:
-            value = value.lower() == 'true'
+            value = (value.lower() == 'true')
         fields[key] = value
     return scheme, fields
 
 def basic_auth():
-    '''
-    Basic access authentication.
 
-    Returns
-    -------
-    str
-        Authorization header.
     '''
-    username = config['username']
-    password = config['password']
-    result = base64.b64encode((username + ':' + password).encode()).decode()
+    basic access authentication;
+
+    ## return
+
+    :str
+    :   authorization header;
+    '''
+
+    plain = conf.username + ':' + conf.password
+    result = base64.b64encode(plain.encode()).decode()
     return 'Basic ' + result
 
 def digest_auth(fields):
+
     '''
-    Digest access authentication.
+    digest access authentication;
 
-    Parameters
-    ----------
-    fields
-        Authentication header fields.
+    ## params
 
-    Returns
-    -------
-    str
-        Authorization header.
+    fields:
+    :   authentication header fields;
+
+    ## return
+
+    :str
+    :   authorization header;
     '''
-    username = config['username']
-    realm = fields['realm']
-    password = config['password']
-    HA1 = md5('{}:{}:{}'.format(username, realm, password))
-    nonce = fields['nonce']
-    nc = '00000001'
-    cnonce = hex(random.randint(0, 0xffffffff))[2:]
-    qop = fields['qop']
-    dest_host = config['dest_host']
-    dest_port = config['dest_port']
-    HA2 = md5('{}:{}:{}'.format('CONNECT', dest_host, str(dest_port)))
-    response = md5(':'.join((HA1, nonce, nc, cnonce, qop, HA2)))
 
-    return (
-        'Digest ' 'username="{username}", ' 'realm="{realm}", '
-        'nonce="{nonce}", ' 'uri="{dest_host}:{dest_port}", ' 'qop={qop}, '
-        'nc={nc}, ' 'cnonce="{cnonce}", ' 'response="{response}"'
-    ).format(
-        username=username, realm=realm, nonce=nonce, dest_host=dest_host,
-        dest_port=dest_port, qop=qop, nc=nc, cnonce=cnonce, response=response)
+    realm = fields.get('realm')
+    nonce = fields.get('nonce')
+    qop = fields.get('qop')
 
-def build_request_head(auth_header):
+    username = conf.username
+    password = conf.password
+    dest_host = conf.dest_host
+    dest_port = conf.dest_port
+
+    ha1 = md5(':'.join((username, realm, password)))
+    ha2 = md5(':'.join(('CONNECT', dest_host, str(dest_port))))
+
+    if qop == 'auth':
+        nc = '00000001'
+        cnonce = '{:x}'.format(random.randint(0, 0xffffffff))
+        response = md5(':'.join((ha1, nonce, nc, cnonce, qop, ha2)))
+    else:
+        response = md5(':'.join((ha1, nonce, ha2)))
+
+    return 'Digest ' + ', '.join((
+        f'username="{username}"',
+        f'realm="{realm}"',
+        f'nonce="{nonce}"',
+        f'qop="{qop}"',
+        f'nc="{nc}"',
+        f'cnonce="{cnonce}"',
+        f'response="{response}"',
+        f'uri="{dest_host}:{dest_port}"',
+    ))
+
+def build_connect_req(auth):
+
     '''
-    Build request head using authorization header.
+    build *connect* request;
 
-    Parameters
-    ----------
-    auth_header
-        Authorization header.
+    ## params
 
-    Returns
-    -------
-    bytes
-        Request head.
+    auth:str
+    :   authorization header;
+
+    ## return
+
+    :bytes
+    :   *connect* request;
     '''
-    uri = '{}:{}'.format(config['dest_host'], config['dest_port'])
-    head = ('CONNECT {uri} HTTP/1.0\n' 'Host: {uri}\n').format(uri=uri)
-    if auth_header is not None:
+
+    uri = '{}:{}'.format(conf.dest_host, conf.dest_port)
+
+    head = (
+        'CONNECT {uri} HTTP/1.0\n'
+        'Host: {uri}\n'
+    ).format(uri=uri)
+
+    if auth is not None:
         head += (
-            'Proxy-Authorization: {auth_header}\n'
-            '\n'
-        ).format(auth_header=auth_header)
-    head = head.encode()
-    return head
+            'Proxy-Authorization: {auth}\n'
+        ).format(auth=auth)
 
-def send_request(head):
-    '''
-    Send CONNECT request to proxy and start forwarding data.
+    return (head + '\n').encode()
 
-    Parameters
-    ----------
-    head
-        Request head.
+def main_loop(req):
+
     '''
+    send *connect* request to proxy and relay data;
+
+    ## params
+
+    req:bytes
+    :   *connect* request;
+    '''
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((config['proxy_host'], config['proxy_port']))
-        sock.sendall(head)
 
+        ##  send *connect* request;
+        sock.connect((conf.proxy_host, conf.proxy_port))
+        sock.sendall(req)
+
+        ##  relay data;
         while True:
             ready, _, _ = select.select([sock, sys.stdin], [], [])
             if sock in ready:
-                data = sock.recv(BUFSIZE)
-                if len(data) == 0:
-                    break
+                data = sock.recv(conf.buf_size)
+                if len(data) == 0: break
                 os.write(sys.stdout.fileno(), data)
             if sys.stdin in ready:
-                data = os.read(sys.stdin.fileno(), BUFSIZE)
-                if len(data) == 0:
-                    break
+                data = os.read(sys.stdin.fileno(), conf.buf_size)
+                if len(data) == 0: break
                 sock.sendall(data)
 
-def run():
-    '''
-    Run this script.
-    '''
-    # Parse arguments.
-    parse_args()
-    verbose = config['verbose']
-    if verbose:
-        print(config)
-
-    # Initial probe.
-    resp = probe()
-    if verbose:
-        print(resp.getheaders())
-
-    # See if auth is needed.
-    if resp.status == 407:
-        auth_header = resp.getheader('Proxy-Authenticate')
-        scheme, fields = parse_auth_header(auth_header)
-        if scheme == 'basic':
-            auth_header = basic_auth()
-        elif scheme == 'digest':
-            auth_header = digest_auth(fields)
-        else:
-            raise Exception('unknown auth scheme')
-    elif resp.status // 100 * 100 == 200:
-        auth_header = None
-    else:
-        raise Exception('Error: {} {}'.format(resp.status, resp.reason))
-
-    # Make request head.
-    head = build_request_head(auth_header)
-    if verbose:
-        print(head)
-
-    # Send the request.
-    send_request(head)
-
-def usage():
-    '''
-    Display usage text.
-    '''
-    config['parser'].print_help()
-
 def main():
+
     '''
-    Main function.
+    main function;
     '''
-    try:
-        run()
-    except Exception:
-        traceback.print_exc()
-        usage()
-        sys.exit(1)
+
+    ##  parse args;
+    args = parse_args()
+
+    ##  config logging;
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    ##  update config;
+    conf.proxy_host = args.proxy_host
+    conf.proxy_port = args.proxy_port
+    conf.dest_host  = args.dest_host
+    conf.dest_port  = args.dest_port
+    conf.auth_file  = args.auth_file
+    conf.buf_size   = args.buf_size
+
+    ##  read auth file;
+    if isfile(args.auth_file):
+        with open(args.auth_file, 'rt') as f:
+            conf.username, conf.password = f.readline().strip().split(':')
+
+    for k, v in conf.__dict__.items():
+        logging.debug('{}={}'.format(k, v))
+
+    ##  probe proxy to check if auth is needed;
+    resp = probe_proxy()
+    logging.debug(resp.getheaders())
+    if resp.status == 407:
+        auth = resp.getheader('Proxy-Authenticate')
+        scheme, fields = parse_auth_header(auth)
+        if scheme == 'basic':
+            auth = basic_auth()
+        elif scheme == 'digest':
+            auth = digest_auth(fields)
+        else:
+            raise Exception('unknown auth scheme: {};'.format(scheme))
+    elif resp.status // 100 * 100 == 200:
+        auth = None
+    else:
+        raise Exception(
+            'connection error: {} {}'.format(resp.status, resp.reason))
+
+    ##  build *connect* request;
+    req = build_connect_req(auth)
+    logging.debug(req)
+
+    ##  send request and enter main loop;
+    main_loop(req)
 
 if __name__ == '__main__':
     main()
